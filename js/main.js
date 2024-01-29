687350daade688c4ecfeac470c649ab4d0c388f5
Yulia Kornilova <gr421_koyuko@lab-407-10.tomtit-tomsk.ru>
2024-01-29 13:59:25 +0700

    практическая работа 2 добавление на страницу ссылки

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks",
-        desk : "sad"
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+
     }
 })
