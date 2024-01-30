2167d30dc9fc08a0ba6f1baf5ff74dc781fd0bd2
Yulia Kornilova <gr421_koyuko@lab-407-10.tomtit-tomsk.ru>
2024-01-30 14:39:47 +0700

    практическая работа 7 добавление в объект с данными свойство onSale

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 let app = new Vue({
     el: '#app',
     data: {
-        brand: "Vue Mastery Socks",
+        brand: "Vue Mastery",
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        selectedVariant: 0,
+        onSale: true,
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg"
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         cart: 0,
@@ -25,14 +27,28 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
-        }
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if(this.onSale === true) {
+                return this.brand + ' ' + this.product + ' on sale';
+            }
+        },
+
     }
 
 
